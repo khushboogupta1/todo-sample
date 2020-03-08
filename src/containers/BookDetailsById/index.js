02109305a79f61ff746b5613765b491edf73ac35
khushboogupta1 <gkhushboo73@gmail.com>
2020-03-08 21:30:18 +0530

    create new book and display respective tasks

--- src/containers/BookDetailsById/index.js
+++ src/containers/BookDetailsById/index.js
@@ -1,27 +1,41 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { BOOKBYID_URL } from "../../constants";
+import { BOOKBYID_URL,BOOKTASKS_URL } from "../../constants";
 import { withRouter } from "react-router-dom";
 
 const BookDetailsById = ({ match }) => {
   console.log(match);
   const bookId = match.params.id;
   const [book, setBook] = useState(null);
-  useEffect(() => {
+  const [tasks, setTasks] = useState([]);
+  
     async function fetchData() {
       const url = `${BOOKBYID_URL}/${bookId}`;
       const rspBook = await axios.get(url);
-      console.log("Book fetched", rspBook);
       setBook(rspBook.data[0]);
+
+      const taskUrl = `${BOOKTASKS_URL}/${bookId}`;
+      const bookTasks = await axios.get(taskUrl);
+      console.log("API tasks",bookTasks);
+      const apiTasks =  bookTasks.data;
+      console.log("var api tasks",apiTasks);
+      setTasks(apiTasks);
     }
+  useEffect(() => {
     fetchData();
   }, [bookId]);
 
   return (
-    <div>
+    <div className="container">
       BookdetailsID
       <h3>ID </h3>
       <div>{(book && book.name) || "NA"}</div>
+      <div>
+        { tasks.map(t => {
+            <div> {t.id} </div>
+          })
+        }
+      </div>
     </div>
   );
 };
@@ -32,3 +46,12 @@ export default withRouter(BookDetailsById);
 // =>
 // useEffect
 // this.setState({ book: ... })
+
+
+{/* <div> { tasks.map(tasks => { 
+    <div key={tasks.id}> {tasks.id} - {tasks.taskName} 
+      
+      </div> 
+    }) 
+  } 
+</div> */}
