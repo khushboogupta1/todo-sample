134ad08ff56e057e37aaa522687795011765010c
khushboogupta1 <gkhushboo73@gmail.com>
2020-05-26 12:13:18 +0530

    Added menu list for books

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,6 @@
 // export const BASE_URL = 'https://5e2dc5843b0d640014be0f64.mockapi.io/api';
-export const BASE_URL = 'https://task-book.000webhostapp.com/index.php';
+////export const BASE_URL = 'https://task-book.000webhostapp.com/index.php';
+export const BASE_URL = 'http://127.0.0.1/index.php/';
 
 export const BOOKS_URL = `${BASE_URL}/getBooks`;
 export const BOOKBYID_URL = `${BASE_URL}/getBookById`;
@@ -9,3 +10,9 @@ export const DELETEBOOK_URL = `${BASE_URL}/deleteBook`;
 export const BOOKTASKS_URL = `${BASE_URL}/getBookTasks`;
 export const CREATETASK_URL = `${BASE_URL}/createTask`;
 export const DELETETASK_URL = `${BASE_URL}/deleteTasks`;
+
+
+
+// WebDev: styles_iss-guide.css
+// For web developers only: CSS specific to the ISS Reference Guide module
+// styles_iss-guidecss - slug
